74fa5291b9e21f08b0ebb5cab536ff6f0df7247f
estefanopobletett <estefanopoblete@liceovvh.cl>
2026-04-13 08:13:10 -0400

    fs
    dgg

---
+++ 01_introduccion/hola_mundo/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("hola bmund, desde archivo externo!...");
+
+// 2) hola mundo con variable y const.
+const saludo = "hola a todos, ";
+let destinatario = "estudiantes";
+
+console.log(saludo + "para los " + destinatario);
+
+// 2) saluddar a una persona ingresada por su teclado.
+let nombre = prompt("ingrese su nombre: ");
+console.log("hola, " + nombre);
---
+++ 02_tipos_de_datos/1.-tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexion con js correcta");
+/*
+dati: tipotexto (string)
+concatenacin de texto con el signo +
+podemos unir texto y variables
+*/
+const nombre = "Estefano";
+const apellido = "Poblete";
+
+console.log("hola, mi nombre es: " + nombre + " " + apellido)
